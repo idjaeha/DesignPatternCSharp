7e59a149354c9d7f6558c7006bed5bfd6a5fce5e
idjaeha <jaehanim1@gmail.com>
2020-07-30 17:05:02 +0900

    add strategy pattern

---
+++ MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.DesignPattern
+{
+    interface IPattern
+    {
+        public void Start();
+    }
+
+    class MainApp
+    {
+        private static IPattern pattern;
+        public static void Main(String[] args)
+        {
+            pattern = new Strategy();
+            pattern.Start();
+        }
+    }
+}
---
+++ Strategy.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Text;
+
+namespace ConsoleApp1.DesignPattern
+{
+    class Item
+    {
+        public int Price { get; set; }
+    }
+
+    interface IDiscountStrategy
+    {
+        public int GetDiscountPrice(Item item);
+    }
+
+    class FirstGuestDiscountStrategy : IDiscountStrategy
+    {
+        private const double discount = 0.9;
+        public int GetDiscountPrice(Item item)
+        {
+            return (int)(item.Price * discount);
+        }
+    }
+
+    class NonFreshItemDiscountStrategy : IDiscountStrategy
+    {
+        private const double discount = 0.8;
+        public int GetDiscountPrice(Item item)
+        {
+            return (int)(item.Price * discount);
+        }
+    }
+
+    class Calculator
+    {
+        private IDiscountStrategy discountStrategy;
+
+        public Calculator(IDiscountStrategy discountStrategy)
+        {
+            this.discountStrategy = discountStrategy;
+        }
+
+        public int Calculate(List<Item> items)
+        {
+            int sum = 0;
+            foreach(Item item in items)
+            {
+                sum += discountStrategy.GetDiscountPrice(item);
+            }
+            return sum;
+        }
+    }
+
+    class Strategy : IPattern
+    {
+        private IDiscountStrategy discountStrategy;
+        private List<Item> items;
+        private const int itemsSize = 10;
+
+        private List<Item> CreateItems()
+        {
+            List<Item> newItems = new List<Item>();
+
+            for(int idx = 0; idx < itemsSize; idx++)
+            {
+                newItems.Add(new Item() { Price = 1000 });
+            }
+
+            return newItems;
+        }
+
+        public void Start()
+        {
+            discountStrategy = new FirstGuestDiscountStrategy();
+            Calculator calculator = new Calculator(discountStrategy);
+            
+            items = CreateItems();
+            Console.WriteLine(calculator.Calculate(items));
+        }
+    }
+}
