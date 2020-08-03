2c3904a84de961bcb455e87a4c14a2d590f6d863
idjaeha <jaehanim1@gmail.com>
2020-08-03 16:30:47 +0900

    add state pattern

--- MainApp.cs
+++ MainApp.cs
@@ -14,7 +14,7 @@ namespace ConsoleApp1.DesignPattern
         private static IPattern pattern;
         public static void Main(String[] args)
         {
-            pattern = new TemplateDeformation();
+            pattern = new State();
             pattern.Start();
         }
     }
---
+++ State.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.DesignPattern
+{
+    interface IPeopleState
+    {
+        public void Response();
+    }
+
+    class AngryState : IPeopleState
+    {
+        public void Response()
+        {
+            Console.WriteLine("Go away!!!");
+        }
+    }
+
+    class HappyState : IPeopleState
+    {
+        public void Response()
+        {
+            Console.WriteLine("Hello~~~ ^^");
+        }
+    }
+
+    class People
+    {
+        private IPeopleState state;
+
+        public IPeopleState State
+        {
+            set
+            {
+                this.state = value;
+            }
+        }
+
+        public People(IPeopleState state)
+        {
+            this.state = state;
+        }
+
+        public void SayHello()
+        {
+            state.Response();
+        }
+    }
+
+    class State : IPattern
+    {
+        public void Start()
+        {
+            People people = new People(new AngryState());
+            people.SayHello();
+
+            people.State = new HappyState();
+            people.SayHello();
+        }
+    }
+}
